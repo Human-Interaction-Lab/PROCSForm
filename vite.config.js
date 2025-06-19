680dcf3f51e296b1b7ed0aeaee67391f96f5556f
Tyson Barrett <t.barrett88@gmail.com>
2025-06-18 22:18:34 -0600

    fix url

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'https://human-interaction-lab.github.io/audio-recorder/',
+  base: 'https://human-interaction-lab.github.io/PROCSForm/',
   build: {
     outDir: 'docs'
   },
